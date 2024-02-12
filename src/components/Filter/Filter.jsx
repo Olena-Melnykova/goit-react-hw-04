f5fe1ed8dfff34274724aac7d9b36519ea299a90
Olena Melnykova <elena88melnykova@gmail.com>
2024-02-12 23:34:31 +0200

    main

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,3 @@
-// import { Component } from "react";
-// import { nanoid } from "nanoid";
-// import {Input, Label, FormStyled} from '../Form/Form.styled'
-
-
-// export default class Filter extends Component {
-//     filterId = nanoid();
-
-//     render() {
-//         const {value, onChange} = this.props;
-//         return (
-//             <FormStyled>
-//             <Label htmlFor="">
-//                 Find contacts by name:
-//             </Label>
-//                 <Input
-//                     id={this.filterId}   
-//                     type="text"
-//                     name="filter"
-//                     value={value}
-//                     onChange={onChange}
-//                     />
-//             </FormStyled>
-//         )
-//     }
-// }
-
 import { Component } from "react";
 import { nanoid } from "nanoid";
 import {Input, Label, FormStyled} from '../Form/Form.styled'
