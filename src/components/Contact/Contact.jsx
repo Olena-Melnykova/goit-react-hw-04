511e9fc816d0d796f446cfdfdc2566f1f46645f1
Olena Melnykova <elena88melnykova@gmail.com>
2024-02-25 23:47:33 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 import { ContactContainer, ContactName, Button } from './Contact.styled.jsx'; 
 
-const Contact = ({ name, number, onDelete }) => (
+const Contact = ({ id, name, number, onDelete }) => (
   <ContactContainer>
     <ContactName>{name}: {number}</ContactName>
-    <Button onClick={onDelete}>Delete</Button>
+    <Button onClick={() => onDelete(id)}>Delete</Button>
   </ContactContainer>
 );
 
 Contact.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
   onDelete: PropTypes.func.isRequired,
